3064af712cda4fc1096619453b16f2cbc8b5811c
helen <1635161916@qq.com>
2026-05-22 13:37:41 +0800

    优化光标位置设置逻辑并更新.gitignore
    - 在 `.gitignore` 中新增对 `FodyWeavers.xsd` 的忽略规则。
    - 调整 `MedicalVizService::SetCursorWorldPosition` 方法：
      - 移除对 `axis` 参数范围的检查，仅保留必要依赖检查。
      - 增加对 `axis == -1` 的特殊处理逻辑。
      - 保留其他 `axis` 值的原有逻辑，优化代码结构。

--- MVVCVTK/src/Service/AppService.cpp
+++ MVVCVTK/src/Service/AppService.cpp
@@ -419,16 +419,21 @@ void MedicalVizService::SetSliceScrolled(int delta)
 
 void MedicalVizService::SetCursorWorldPosition(double worldPos[3], int axis)
 {
-    if (!m_dataManager || !m_dataManager->GetVtkImage() || !m_sharedState || axis < 0 || axis > 2) return;
+    if (!m_dataManager || !m_dataManager->GetVtkImage() || !m_sharedState) return;
     auto currentPos = m_sharedState->GetCursorWorld();
     m_sharedState->SetCursorRawWorld(worldPos[0], worldPos[1], worldPos[2]);
     m_sharedState->SetCursorAxis(axis);
 
     // 0 1 2 分别表示 SliceLeft_right SliceFront_back SliceTop_down
-    double newPos[3] = { worldPos[0], worldPos[1], worldPos[2] };
-    newPos[axis] = currentPos[axis];
-
-    m_sharedState->SetCursorWorld(newPos[0], newPos[1], newPos[2]);
+    if (axis == -1)
+    {
+        m_sharedState->SetCursorWorld(worldPos[0], worldPos[1], worldPos[2]);
+    }
+    else {
+        double newPos[3] = { worldPos[0], worldPos[1], worldPos[2] };
+        newPos[axis] = currentPos[axis];
+        m_sharedState->SetCursorWorld(newPos[0], newPos[1], newPos[2]);
+    }
 }
 
 std::array<double, 3> MedicalVizService::GetCursorWorld()
